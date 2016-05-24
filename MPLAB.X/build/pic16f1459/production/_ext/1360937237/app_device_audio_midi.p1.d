294cec88a44345e18ed169f134f9ed5e8a295ee1
pvman <pvman@list.ru>
2016-05-24 13:47:52 +0400

    выключена оптимизация

---
+++ MPLAB.X/build/pic16f1459/production/_ext/1360937237/app_device_audio_midi.p1.d
@@ -0,0 +1,22 @@
+ build/pic16f1459/production/_ext/1360937237/app_device_audio_midi.d  \
+ build/pic16f1459/production/_ext/1360937237/app_device_audio_midi.p1:  \
+ ../src/app_device_audio_midi.c  \
+../src/../framework/usb/usb_common.h  \
+../src/../framework/usb/usb_device_midi.h  \
+../src/../framework/usb/usb_hal_pic16f1.h  \
+../src/usb_config.h  \
+../src/../src/pic16f1459/io_mapping.h  \
+../src/../framework/usb/usb_ch9.h  \
+../src/../bsp/pic16f1459/leds.h  \
+../src/app_device_ble_midi.h  \
+../src/pic16f1459/system.h  \
+../src/../src/pic16f1459/system.h  \
+../src/../framework/usb/usb.h  \
+../src/../framework/usb/usb_device.h  \
+../src/../src/system_config.h  \
+../src/../bsp/pic16f1459/power.h  \
+../bsp/pic16f1459/buttons.h  \
+../bsp/pic16f1459/leds.h  \
+../src/../bsp/pic16f1459/buttons.h  \
+../src/system_config.h  \
+../src/../src/pic16f1459/fixed_address_memory.h 
---
+++ MPLAB.X/build/pic16f1459/production/_ext/867131293/leds.p1.d
@@ -0,0 +1,4 @@
+ build/pic16f1459/production/_ext/867131293/leds.d  \
+ build/pic16f1459/production/_ext/867131293/leds.p1:  \
+ ../bsp/pic16f1459/leds.c  \
+../bsp/pic16f1459/leds.h 
