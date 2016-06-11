9c294bc6137b2da6fb4664cb1b243311f0cedd83
pvman <pvman@list.ru>
2016-06-11 13:35:19 +0400

    по USB обнаруживается

--- MPLAB.X/nbproject/Makefile-pic16f1459.mk
+++ MPLAB.X/nbproject/Makefile-pic16f1459.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../src/app_led_usb_status.c ../src/main.c ../src/usb_descriptors.c ../src/app_device_audio_midi.c ../src/scan.c ../src/pic16f1459/system.c ../bsp/pic16f1459/buttons.c ../bsp/pic16f1459/adc.c ../bsp/pic16f1459/leds.c ../bsp/pic16f1459/usart.c ../framework/usb/usb_device.c
+SOURCEFILES_QUOTED_IF_SPACED=../src/pic16f1459/system.c ../src/app_led_usb_status.c ../src/main.c ../src/usb_descriptors.c ../src/app_device_audio_midi.c ../src/scan.c ../bsp/pic16f1459/buttons.c ../bsp/pic16f1459/adc.c ../bsp/pic16f1459/leds.c ../bsp/pic16f1459/usart.c ../framework/usb/usb_device.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1 ${OBJECTDIR}/_ext/1360937237/main.p1 ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1 ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1 ${OBJECTDIR}/_ext/1360937237/scan.p1 ${OBJECTDIR}/_ext/522103080/system.p1 ${OBJECTDIR}/_ext/867131293/buttons.p1 ${OBJECTDIR}/_ext/867131293/adc.p1 ${OBJECTDIR}/_ext/867131293/leds.p1 ${OBJECTDIR}/_ext/867131293/usart.p1 ${OBJECTDIR}/_ext/1986338844/usb_device.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1.d ${OBJECTDIR}/_ext/1360937237/main.p1.d ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1.d ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1.d ${OBJECTDIR}/_ext/1360937237/scan.p1.d ${OBJECTDIR}/_ext/522103080/system.p1.d ${OBJECTDIR}/_ext/867131293/buttons.p1.d ${OBJECTDIR}/_ext/867131293/adc.p1.d ${OBJECTDIR}/_ext/867131293/leds.p1.d ${OBJECTDIR}/_ext/867131293/usart.p1.d ${OBJECTDIR}/_ext/1986338844/usb_device.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/522103080/system.p1 ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1 ${OBJECTDIR}/_ext/1360937237/main.p1 ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1 ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1 ${OBJECTDIR}/_ext/1360937237/scan.p1 ${OBJECTDIR}/_ext/867131293/buttons.p1 ${OBJECTDIR}/_ext/867131293/adc.p1 ${OBJECTDIR}/_ext/867131293/leds.p1 ${OBJECTDIR}/_ext/867131293/usart.p1 ${OBJECTDIR}/_ext/1986338844/usb_device.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/522103080/system.p1.d ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1.d ${OBJECTDIR}/_ext/1360937237/main.p1.d ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1.d ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1.d ${OBJECTDIR}/_ext/1360937237/scan.p1.d ${OBJECTDIR}/_ext/867131293/buttons.p1.d ${OBJECTDIR}/_ext/867131293/adc.p1.d ${OBJECTDIR}/_ext/867131293/leds.p1.d ${OBJECTDIR}/_ext/867131293/usart.p1.d ${OBJECTDIR}/_ext/1986338844/usb_device.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1 ${OBJECTDIR}/_ext/1360937237/main.p1 ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1 ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1 ${OBJECTDIR}/_ext/1360937237/scan.p1 ${OBJECTDIR}/_ext/522103080/system.p1 ${OBJECTDIR}/_ext/867131293/buttons.p1 ${OBJECTDIR}/_ext/867131293/adc.p1 ${OBJECTDIR}/_ext/867131293/leds.p1 ${OBJECTDIR}/_ext/867131293/usart.p1 ${OBJECTDIR}/_ext/1986338844/usb_device.p1
+OBJECTFILES=${OBJECTDIR}/_ext/522103080/system.p1 ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1 ${OBJECTDIR}/_ext/1360937237/main.p1 ${OBJECTDIR}/_ext/1360937237/usb_descriptors.p1 ${OBJECTDIR}/_ext/1360937237/app_device_audio_midi.p1 ${OBJECTDIR}/_ext/1360937237/scan.p1 ${OBJECTDIR}/_ext/867131293/buttons.p1 ${OBJECTDIR}/_ext/867131293/adc.p1 ${OBJECTDIR}/_ext/867131293/leds.p1 ${OBJECTDIR}/_ext/867131293/usart.p1 ${OBJECTDIR}/_ext/1986338844/usb_device.p1
 
 # Source Files
-SOURCEFILES=../src/app_led_usb_status.c ../src/main.c ../src/usb_descriptors.c ../src/app_device_audio_midi.c ../src/scan.c ../src/pic16f1459/system.c ../bsp/pic16f1459/buttons.c ../bsp/pic16f1459/adc.c ../bsp/pic16f1459/leds.c ../bsp/pic16f1459/usart.c ../framework/usb/usb_device.c
+SOURCEFILES=../src/pic16f1459/system.c ../src/app_led_usb_status.c ../src/main.c ../src/usb_descriptors.c ../src/app_device_audio_midi.c ../src/scan.c ../bsp/pic16f1459/buttons.c ../bsp/pic16f1459/adc.c ../bsp/pic16f1459/leds.c ../bsp/pic16f1459/usart.c ../framework/usb/usb_device.c
 
 
 CFLAGS=
@@ -81,6 +81,14 @@ MP_PROCESSOR_OPTION=16F1459
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/522103080/system.p1: ../src/pic16f1459/system.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/522103080" 
+	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/522103080/system.p1  ../src/pic16f1459/system.c 
+	@-${MV} ${OBJECTDIR}/_ext/522103080/system.d ${OBJECTDIR}/_ext/522103080/system.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/522103080/system.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1: ../src/app_led_usb_status.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1360937237" 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1.d 
@@ -120,14 +128,6 @@ ${OBJECTDIR}/_ext/1360937237/scan.p1: ../src/scan.c  nbproject/Makefile-${CND_CO
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1360937237/scan.p1  ../src/scan.c 
 	@-${MV} ${OBJECTDIR}/_ext/1360937237/scan.d ${OBJECTDIR}/_ext/1360937237/scan.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1360937237/scan.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/522103080/system.p1: ../src/pic16f1459/system.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/522103080" 
-	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/522103080/system.p1  ../src/pic16f1459/system.c 
-	@-${MV} ${OBJECTDIR}/_ext/522103080/system.d ${OBJECTDIR}/_ext/522103080/system.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/522103080/system.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 ${OBJECTDIR}/_ext/867131293/buttons.p1: ../bsp/pic16f1459/buttons.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/867131293" 
@@ -170,6 +170,14 @@ ${OBJECTDIR}/_ext/1986338844/usb_device.p1: ../framework/usb/usb_device.c  nbpro
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1986338844/usb_device.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
+${OBJECTDIR}/_ext/522103080/system.p1: ../src/pic16f1459/system.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/522103080" 
+	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/522103080/system.p1  ../src/pic16f1459/system.c 
+	@-${MV} ${OBJECTDIR}/_ext/522103080/system.d ${OBJECTDIR}/_ext/522103080/system.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/522103080/system.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1: ../src/app_led_usb_status.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/1360937237" 
 	@${RM} ${OBJECTDIR}/_ext/1360937237/app_led_usb_status.p1.d 
@@ -209,14 +217,6 @@ ${OBJECTDIR}/_ext/1360937237/scan.p1: ../src/scan.c  nbproject/Makefile-${CND_CO
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1360937237/scan.p1  ../src/scan.c 
 	@-${MV} ${OBJECTDIR}/_ext/1360937237/scan.d ${OBJECTDIR}/_ext/1360937237/scan.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1360937237/scan.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/_ext/522103080/system.p1: ../src/pic16f1459/system.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} "${OBJECTDIR}/_ext/522103080" 
-	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/522103080/system.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 -I"../src" -I"../src/pic16f1459" -I"../framework/usb" -I"../bsp/pic16f1459" --warn=-3 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/522103080/system.p1  ../src/pic16f1459/system.c 
-	@-${MV} ${OBJECTDIR}/_ext/522103080/system.d ${OBJECTDIR}/_ext/522103080/system.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/522103080/system.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 ${OBJECTDIR}/_ext/867131293/buttons.p1: ../bsp/pic16f1459/buttons.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/867131293" 
